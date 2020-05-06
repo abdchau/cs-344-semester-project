818a7152916461493140080a8c7abed4bcab4d6d
System Administrator <root@macbooks-MacBook-Pro.local>
2020-05-06 21:14:34 +0500

    Inserting dummy data

--- controller/users.js
+++ controller/users.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.src = '../controller/jquery.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
@@ -29,26 +29,18 @@ function verifyUser(email, password){
 	return email+password;
 }
 
-function addUser(email, password, firstName, lastName, address){
+function addUser(email, password, firstName, lastName, address, city, zipcode){
 	$.ajax({
 		type:'POST', 
 		url: '../model/users.php',
-		data: {'email':email, 'password':password, 'func':'resetDB'},
+		data: {'email':email, 'password':password, 'firstName':firstName, 'lastName':lastName,
+			'address':address, 'city':city, 'zipcode':zipcode, 'func':'addUser'},
 		datatype: 'json',
 		success: function(response){
 
 			console.log('post success');
 			console.log(response);
-			// response = JSON.parse(response);
-			// if (response===null) {
-			// 	console.log("Email incorrect");
-			// 	return;
-			// }
 
-			// if(password===response['password'])
-			// 	console.log('Sign in successful');
-			// else
-			// 	console.log('Password incorrect');
 		},
 		error: function(){console.log('post error');}
 	})
