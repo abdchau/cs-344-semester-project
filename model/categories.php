1264812c0ed0a14eeaa5fbcc25284b5c703c654b
abdchau <abdullahchaudhry0@outlook.com>
2020-05-08 00:50:07 +0500

    Category backend added

--- model/categories.php
+++ model/categories.php
@@ -1,5 +1,30 @@
-{ "records":[{"id":1, "name":"Phones"},
- {"id":2, "name":"Laptops"},
- {"id":3, "name":"TVs"},
- {"id":4, "name":"Home Appliances"}
- ]}
+<?php
+
+require 'connect.php';
+
+function getCategories($conn){
+	$result = $conn->query("select * from shopping.categories");
+	if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $arr[] = $row;
+    }
+}
+
+	return json_encode($arr).$conn->error;
+}
+
+$conn = connectDB();
+$post = json_decode(file_get_contents('php://input'));
+// header('Content-Type: application/json');
+if (isset($post)){
+	if ($post->func=='getCategories'){
+
+		echo getCategories($conn);
+	}
+}
+else
+	echo "No function specified".$post->func;
+// echo getCategories($conn);
+
+?>
